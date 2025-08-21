bcaf925ab2b13291237b84bbc88d3ab5264b9634
ebt-wu <edbertwu08@gmail.com>
2025-08-21 13:40:18 +0800

    Level-1

--- src/main/java/Eggy.java
+++ src/main/java/Eggy.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
+
 public class Eggy {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String line = "____________________________________________________________";
         String formatted_string = String.format(
-                "%s\nHello! I'm EGGY\nWhat can I do for you?\n%s\nBye. Hope to see you again soon!\n%s", line, line,
-                line);
+                "%s\nHello! I'm EGGY\nWhat can I do for you?\n%s\n", line, line);
         System.out.println(formatted_string);
+        String current = "";
+        while (!current.equals("bye")) {
+            current = sc.nextLine();
+            System.out.println(String.format("\n%s\n%s\n%s", line, current, line));
+        }
+        System.out.println("Bye. Hope to see you again soon!");
+        sc.close();
     }
 }
