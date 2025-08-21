e0aaec8c6bcfa7b87eae7e9747cc05e3f75c2960
ebt-wu <edbertwu08@gmail.com>
2025-08-22 02:00:55 +0800

    Level-3

--- src/main/java/Eggy.java
+++ src/main/java/Eggy.java
@@ -2,35 +2,59 @@ import java.util.Scanner;
 
 public class Eggy {
     public static int count = 0;
-    public static String[] list = new String[100];
+    public static Task[] list = new Task[100];
     public static String current = "";
+
+    public static Task handleMarkUnmark(String command) {
+        String[] parts = command.split(" ");
+        int index;
+        index = Integer.parseInt(parts[1]) - 1;
+        Task task = list[index];
+        task.changeMark();
+        System.out.println("    " + task);
+        return task;
+    }
 
     public static void toString(Scanner sc) {
         String line = "____________________________________________________________";
         while (!current.equals("bye")) {
             current = sc.nextLine();
-            if (!current.equals("list")) {
-                append(current);
-                System.out.println(String.format("\n%s\nadded: %s\n%s", line, current, line));
-            } else {
+            if (current.equals("list")) {
                 System.out.println(getStringInList(list) + String.format("%s", line));
+            } else if (current.startsWith("mark ")) {
+                System.out.println("\n" + line);
+                System.out.println("Nice! I've marked this task as done:\n");
+                handleMarkUnmark(current);
+                System.out.println("\n" + line);
+            } else if (current.startsWith("unmark ")) {
+                System.out.println("\n" + line);
+                System.out.println("OK, I've marked this task as not done yet:\n");
+                handleMarkUnmark(current);
+                System.out.println("\n" + line);
+            } else {
+                Task re = append(current);
+                System.out.println(String.format("\n%s\nadded: %s\n%s\n", line, re.toString(), line));
             }
         }
     }
 
-    public static void append(String newElement) {
+    public static Task append(String newElement) {
         if (count < list.length) {
-            list[count] = newElement;
+            Task appended = new Task(newElement);
+            list[count] = appended;
             count++;
+            return appended;
         } else {
             System.out.println("Array is full, cannot append new element.");
+            return new Task("");
         }
     }
 
-    public static String getStringInList(String[] list) {
+    public static String getStringInList(Task[] list) {
         String result = "Here are the tasks in your list:\n";
         for (int i = 0; i < count; i++) {
-            result = result + String.format("%d. ", i + 1) + list[i] + "\n";
+            Task task = list[i];
+            result = result + String.format("%d. ", i + 1) + task + "\n";
         }
         return result;
     }
